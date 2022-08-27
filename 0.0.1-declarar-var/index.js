80aa6d7d96b5888ec210d201995769873498da32
arturbazza <agencia@fonzie.com.br>
2022-08-26 21:41:22 -0300

    1 file modified

--- 0.0.1-declarar-var/index.js
+++ 0.0.1-declarar-var/index.js
@@ -1,15 +1,21 @@
 let nome = 'Artur';
-
 let idade = '47';
-
 let vegetariano = false;
-
 console.log(nome, '-', idade, '-', vegetariano);
 
 const conjuntoNumber = [123, 40];
-
 console.log(conjuntoNumber);
 
 const conjuntoVariavel = ('Artur', 47);
-
 console.log(conjuntoVariavel);
+
+let atores = ['Antony Robins', 'Charles Bukowski'];
+console.log(atores);
+
+let numerosPrimos = [2, 3, 5];
+console.log(numerosPrimos);
+
+//nao rodou abaixo - VER
+const valores = ['Artur', 47, null];
+const nome = valores[0];
+console.log(nome);
