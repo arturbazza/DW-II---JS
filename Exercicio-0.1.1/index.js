dfc9e113fcb51ade199efefb46e740cc19dfb455
arturbazza <104048589+arturbazza@users.noreply.github.com>
2022-09-02 20:22:43 -0300

    1 file modified

--- Exercicio-0.1.1/index.js
+++ Exercicio-0.1.1/index.js
@@ -1,2 +1,8 @@
 //Faça uma função que irá receber como parâmetro dois numeros. O primeiro parâmetro irá representar o valor de um boleto e o segundo parâmetro irá representar a quantidade de dias que essa conta está atrasada. Para cada dia atrasado deve ser cobrado uma taxa de 0,33%. A função deve retornar o novo valor do boleto. Considere o cálculo de juros simples.
 //
+
+function exibirBoleto(valor, diasAtraso) {
+  console.log('Artur, tem ' + idade + ' anos.');
+}
+
+console.log.exibirBoleto();
