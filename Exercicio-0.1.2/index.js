ec93d740433498b8f46882c91ef0fb7def3ca02c
arturbazza <104048589+arturbazza@users.noreply.github.com>
2022-09-02 22:11:07 -0300

    1 file modified

--- Exercicio-0.1.2/index.js
+++ Exercicio-0.1.2/index.js
@@ -2,7 +2,7 @@
 function comprasMercado(valor, cartaoLoja, totalCartaoConvenio) {
   if (cartaoLoja && totalCartaoConvenio) {
     return valor - valor * 0.15;
-  }
+  } elseif ()
 
   console.log((totalCartaoConvenio = valor - valor * 0.1));
 }
