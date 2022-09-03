05fccd1c080dd1ec2ddd87b36a65e4b958ca9c56
arturbazza <104048589+arturbazza@users.noreply.github.com>
2022-09-02 21:47:28 -0300

    1 file modified

--- Exercicio-0.1.2/index.js
+++ Exercicio-0.1.2/index.js
@@ -1,5 +1,12 @@
 //Faça uma função que irá receber três parâmetros. O primeiro será um numero que representa o valor de uma compra que está sendo feita no mercado / farmácia. O segundo parâmetro é um boolean que representa se a compra possui ou não convênio com o estabelecimento e o último parâmetro representa se a compra está sendo feita ou não com o cartão da loja. Se a pessoa que estiver fazendo a compra possuir o cartão da loja e possui convênio então o desconto será de 15%. Se o usuário possuir convenio ou estiver fazendo a compra pelo cartão da loja o desconto é de 10%. Caso o usuário não tenha nenhum desses benefícios não existe desconto para a compra.
 //
+
+function getNome() {
+	return 'mathias';
+}
+getNome(); //mathias
+
+/*
 function comprasMercado(valor, cartaoLoja, totalCartaoConvenio) {
     if (cartaoLoja === true) {
       totalCartaoLoja = valor - (valor * 0.15);
@@ -12,4 +19,4 @@ function comprasMercado(valor, cartaoLoja, totalCartaoConvenio) {
       console.log(totalCartaoConvenio);
     }
   }
-}
+  */
